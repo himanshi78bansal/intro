8c7574f293566ec3504d3fd8baacefbb4ba7f061
Himanshi Bansal <101702840+Himanshi7Bansal@users.noreply.github.com>
2023-07-19 00:28:22 +0530

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+// import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,103 +12,164 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+      SystemChrome.setEnabledSystemUIMode(SystemUiMode.leanBack);
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Color.fromARGB(255, 1, 23, 40),
-        body: Container(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              CircleAvatar(
-                radius: 150,
-                backgroundImage: AssetImage('images/himanshi.jpg'),
-              ),
-              Text(
-                "Himanshi Bansal",
-                style: TextStyle(
-                    fontFamily: 'Pacifico',
-                    fontWeight: FontWeight.bold,
-                    fontSize: 30.0,
-                    color: Colors.white,
-                    letterSpacing: 2.0,
-                    wordSpacing: 4.0),
-              ),
-              Text(
-                "Learner",
-                style: TextStyle(
-                    fontSize: 20.0,
-                    color: Colors.white,
-                    fontFamily: 'Pacifico',
-                    fontWeight: FontWeight.bold),
-              ),
-              Card(
-                color: Colors.white,
-                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-                child: ListTile(
-                  leading: Icon(
-                    Icons.phone,
-                    color: Color.fromARGB(255, 1, 23, 40),
-                  ),
-                  title: Text(
-                    "+91 7497035925",
-                    style: TextStyle(),
-                  ),
-                  trailing: Icon(
-                    Icons.copy,
-                    color: Color.fromARGB(255, 1, 23, 40),
-                  ),
+        backgroundColor: const Color.fromARGB(255, 1, 23, 40),
+        body: SingleChildScrollView(
+          child: Center(
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 30,
                 ),
-              ),
-              Card(
-                color: Colors.white,
-                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-                child: ListTile(
-                  leading: Icon(
-                    Icons.email,
-                    color: Color.fromARGB(255, 1, 23, 40),
-                  ),
-                  title: Text(
-                    "hbnsl78@gmail.com",
-                    style: TextStyle(),
-                  ),
-                  trailing: Icon(
-                    Icons.copy,
-                    color: Color.fromARGB(255, 1, 23, 40),
-                  ),
+                CircleAvatar(
+                  radius: 130,
+                  backgroundImage: AssetImage('images/himanshi.jpg'),
                 ),
-              ),
-              Card(
-                color: Colors.white,
-                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-                child: ListTile(
-                  leading: Center(
-                    child: Text(
-                      "in",
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,fontSize: 20,
-                        color: Color.fromARGB(255, 1, 23, 40),
-                        
-                      ),
+                Text(
+                  "Himanshi Bansal",
+                  style: TextStyle(
+                      fontFamily: 'Pacifico',
+                      fontWeight: FontWeight.bold,
+                      fontSize: 30.0,
+                      color: Colors.white,
+                      letterSpacing: 2.0,
+                      wordSpacing: 4.0),
+                ),
+                Text(
+                  "Learner",
+                  style: TextStyle(
+                      fontSize: 20.0,
+                      color: Colors.white,
+                      fontFamily: 'Pacifico',
+                      fontWeight: FontWeight.bold),
+                ),
+                Card(
+                  color: Colors.white,
+                  margin:
+                      EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.phone,
+                      color: Color.fromARGB(255, 1, 23, 40),
                     ),
-                  ),
-                  title: Text(
-                    "https://www.linkedin.com/in/himanshi-bansal-495196222",
-                    style: TextStyle(),
-                  ),
-                  trailing: GestureDetector(
-                    onTap: () {
-                      CopySelectionTextIntent;
-                    },
-                    child: Icon(
+                    title: Text(
+                      "+91 7497035925",
+                      style: TextStyle(),
+                    ),
+                    trailing: Icon(
                       Icons.copy,
                       color: Color.fromARGB(255, 1, 23, 40),
                     ),
+                    onTap: () async {
+                      await Clipboard.setData(
+                          ClipboardData(text: "7497035925"));
+                      // copied successfully
+                    },
                   ),
                 ),
-              ),
-            ],
+                Card(
+                  color: Colors.white,
+                  margin:
+                      EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.email,
+                      color: Color.fromARGB(255, 1, 23, 40),
+                    ),
+                    title: new InkWell(
+                        child: new Text('hbnsl78@gmail.com'),
+                        onTap: () => launch(
+                            'https://mail.google.com/mail/u/0/?tab=rm&ogbl#inbox')),
+                    trailing: Icon(
+                      Icons.copy,
+                      color: Color.fromARGB(255, 1, 23, 40),
+                    ),
+                    onTap: () async {
+                      await Clipboard.setData(
+                          ClipboardData(text: "hbnsl78@gmail.com"));
+                      // copied successfully
+                    },
+                  ),
+                ),
+                Card(
+                  color: Colors.white,
+                  margin:
+                      EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                  child: ListTile(
+                    leading: Text(
+                      "in",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                          color: Color.fromARGB(255, 1, 23, 40)),
+                    ),
+                    title: new InkWell(
+                        child: new Text(
+                            'https://www.linkedin.com/in/himanshi-bansal-495196222'),
+                        onTap: () => launch(
+                            'https://www.linkedin.com/in/himanshi-bansal-495196222')),
+                    trailing: Icon(
+                      Icons.copy,
+                      color: Color.fromARGB(255, 1, 23, 40),
+                    ),
+                    onTap: () async {
+                      await Clipboard.setData(ClipboardData(
+                          text:
+                              "https://www.linkedin.com/in/himanshi-bansal-495196222"));
+                      // copied successfully
+                    },
+                  ),
+                ),
+                Card(
+                  color: Colors.white,
+                  margin:
+                      EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                  child: ListTile(
+                    leading: Image.asset('images/GitHub.png',
+                        width: 20, height: 20, fit: BoxFit.fill),
+                    title: new InkWell(
+                        child: new Text('https://github.com/Himanshi7Bansal'),
+                        onTap: () =>
+                            launch('https://github.com/Himanshi7Bansal')),
+                    trailing: Icon(
+                      Icons.copy,
+                      color: Color.fromARGB(255, 1, 23, 40),
+                    ),
+                    onTap: () async {
+                      await Clipboard.setData(ClipboardData(
+                          text: "https://github.com/Himanshi7Bansal"));
+                      // copied successfully
+                    },
+                  ),
+                ),
+                Card(
+                  color: Colors.white,
+                  margin:
+                      EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                  child: ListTile(
+                    leading: Image.asset('images/hackerrank.png',
+                        width: 30, height: 30, fit: BoxFit.fill),
+                    title: new InkWell(
+                        child: new Text(
+                            'https://www.hackerrank.com/hbnsl78?hr_r=1'),
+                        onTap: () => launch(
+                            'https://www.hackerrank.com/hbnsl78?hr_r=1')),
+                    trailing: Icon(
+                      Icons.copy,
+                      color: Color.fromARGB(255, 1, 23, 40),
+                    ),
+                    onTap: () async {
+                      await Clipboard.setData(ClipboardData(
+                          text: "https://www.hackerrank.com/hbnsl78?hr_r=1"));
+                      // copied successfully
+                    },
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
